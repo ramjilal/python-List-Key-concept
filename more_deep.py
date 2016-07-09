b107fd391ac77fb4cbd39eb667f356e078adf245
Ramjilal Choudhary <ramjilal@users.noreply.github.com>
2016-07-09 12:15:05 +0530

    Delete more_deep.py

--- more_deep.py
+++
@@ -1,6 +0,0 @@
-x = ['a',['b','c',['d','e']]]
-print x[0]       #print -> 'a'
-print x[1]       #print -> ['b','c',['d','e']]
-print x[1][0]    #print -> 'b'
-print x[1][2]    #print -> ['d','e']
-print x[1][2][0] #print -> 'd'
